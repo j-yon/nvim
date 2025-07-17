0f00961deb18226105d73e089b5ec81765267092
j-yon <jyon7@gatech.edu>
2025-07-16 20:47:52 -0400

    update functions and add gitsigns

--- lua/config/functions.lua
+++ lua/config/functions.lua
@@ -1,6 +1,33 @@
-RunTerm = function(args)
-    vim.cmd("vs")
-    vim.cmd("wincmd l")
-    vim.cmd("lcd " .. vim.fn.expand("%:p:h"))
-    vim.cmd("term " .. args)
+MainPythonFile = nil
+UpdateMainPython = function()
+    MainPythonFile = vim.fn.expand("%:p")
+    print("Main python file updated to: " .. MainPythonFile)
+end
+
+RunMainPython = function()
+    if MainPythonFile == nil then
+        print("No main python file set. Please update it first.")
+    else
+        print("Running main python file: " .. MainPythonFile)
+        vim.cmd("vs")
+        vim.cmd("wincmd l")
+        vim.cmd("term python " .. MainPythonFile)
+    end
+end
+
+MainbashFile = nil
+UpdateMainBash = function()
+    MainBashFile = vim.fn.expand("%:t")
+    print("Main bash file updated to: " .. MainBashFile)
+end
+
+RunMainBash = function()
+    if MainBashFile == nil then
+        print("No Main bash file set. Please update it first.")
+    else
+        print("Running main bash file: " .. MainBashFile)
+        vim.cmd("vs")
+        vim.cmd("wincmd l")
+        vim.cmd("term bash " .. MainBashFile)
+    end
 end
