5cd9d2f94a3c66a4115d1475f968b94ba7790a0a
j-yon <jyon7@gatech.edu>
2026-01-06 17:06:16 -0500

    change bg hl groups for lazy/mason

--- lua/plugins/ui/colors.lua
+++ lua/plugins/ui/colors.lua
@@ -146,12 +146,12 @@ return {
                         -- Save an hlgroup with dark background and dimmed foreground
                         -- so that you can use it where your still want darker windows.
                         -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-                        NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+                        NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_p2 },
 
                         -- Popular plugins that open floats will link to NormalFloat by default;
                         -- set their background accordingly if you wish to keep them dark and borderless
-                        LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-                        MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+                        LazyNormal = { bg = theme.ui.bg_p1, fg = theme.ui.fg_dim },
+                        MasonNormal = { bg = theme.ui.bg_p1, fg = theme.ui.fg_dim },
                     }
                 end,
             })
