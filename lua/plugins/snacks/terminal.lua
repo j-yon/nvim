50f9c5b59ccf421a68db723356d821822e37edff
j-yon <jyon7@gatech.edu>
2026-02-17 21:36:48 -0500

    update terminal behavior

--- lua/plugins/snacks/terminal.lua
+++ lua/plugins/snacks/terminal.lua
@@ -1,21 +1,20 @@
 return {
-    "snacks.nvim",
-    opts = {
-        styles = {
-            terminal = {
-                -- position = "float",
-                -- width = 0.9,
-                stack = true,
-                -- height = 0.3,
-                border = "rounded",
-                backdrop = false,
-                wo = {
-                    winhighlight = "NormalFloat:Normal",
-                },
-            },
-        },
-        terminal = {
-            enabled = true,
-        },
-    },
+    -- "snacks.nvim",
+    -- opts = {
+    --     styles = {
+    --         terminal = {
+    --             position = "current",
+    --             -- width = 0.9,
+    --             -- stack = true,
+    --             -- height = 0.3,
+    --             backdrop = false,
+    --             wo = {
+    --                 winhighlight = "NormalFloat:Normal",
+    --             },
+    --         },
+    --     },
+    --     terminal = {
+    --         enabled = true,
+    --     },
+    -- },
 }
