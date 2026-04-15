56d3dba9a41bd2997fc7340718923666d474f6c2
j-yon <jyon7@gatech.edu>
2026-04-14 21:51:43 -0400

    add dependency for leetcode

--- lua/plugins/misc/leet.lua
+++ lua/plugins/misc/leet.lua
@@ -5,6 +5,7 @@ return {
         -- include a picker of your choice, see picker section for more details
         "nvim-lua/plenary.nvim",
         "MunifTanjim/nui.nvim",
+        "nvim-treesitter/nvim-treesitter",
     },
     opts = {
         -- configuration goes here
